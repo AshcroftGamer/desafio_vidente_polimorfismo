6555f579c670cbaad1f02b3949e6c4fa3c40d5f1
AshcroftGamer <lippeativista@gmail.com>
2021-11-03 14:59:06 -0300

    fim do modulo

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'dart:math';
 void main() {
   runApp(MaterialApp(
     home: VidentVirtualPage(),
@@ -9,11 +9,39 @@ void main() {
 class VidentVirtualPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return Vidente();
+  }
+}
+
+class Vidente extends StatefulWidget {
+  @override
+  _VidenteState createState() => _VidenteState();
+}
+
+class _VidenteState extends State<Vidente> {
+  int numeroDaImagem = 1;
+
+  void _mudaImagem(){
+    setState(() {
+      numeroDaImagem = Random().nextInt(6) + 1;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF332B25),
       appBar: AppBar(
         title: Text('Vidente Virtual'),
         backgroundColor: Colors.brown[800],
+      ),
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            _mudaImagem();
+            print(numeroDaImagem);
+          },
+          child: Image.asset('imagens/vidente$numeroDaImagem.png'),
+        ),
       ),
     );
   }
