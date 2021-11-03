3399b3e375c0e97958e8325eddca004161dae991
AshcroftGamer <lippeativista@gmail.com>
2021-11-03 14:28:59 -0300

    arquivos modificados e criados

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,19 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Container(),
+    home: VidentVirtualPage(),
   ));
+}
+
+class VidentVirtualPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF332B25),
+      appBar: AppBar(
+        title: Text('Vidente Virtual'),
+        backgroundColor: Colors.brown[800],
+      ),
+    );
+  }
 }
